fa907d0b04f23522ef71eb7aacaf1ce601fed8e8
HYEJI <145624152+hyejiya@users.noreply.github.com>
2024-03-22 09:35:05 +0900

    Add_C# Basic

---
+++ C#_Basic/Practice_24_03_21/Practice_01.cs
@@ -0,0 +1,42 @@
+﻿class CharacterController : object
+{
+    public float Hp
+    {
+        get => _hp;
+        set => _hp  = value;
+    }
+
+    private float _hp;
+}
+
+struct ItemData
+{
+    public int Id;
+    public int Num;
+}
+
+internal class Practice_01
+{
+    static void Main(string[] args) //stack : 4byte + 4byte
+    {
+        object obj = new CharacterController(); 
+   
+        obj = new ItemData { Id = 0, Num = 0 }; //boxing
+        ItemData data = (ItemData)obj;//unboxing : 원래 값을 명시해줘야 함
+        //Hip에 저장된 정보를 읽는 것을 unboxing이라고 한다.
+        object obj_2 = obj;
+        
+
+        long a = 10;
+        int b = (int)a;//작은 데이터에 큰 데이터를 넣는 건 명시적 형변환을 해줘야 함.
+        a = b; //큰 데이터에 작은 데이터를 넣는 건 자동으로 변환됨
+
+        StringWriter sw = new StringWriter();
+        sw.Dispose();
+
+        using (StreamWriter streamWriter = new StreamWriter(""))
+        {
+
+        }
+    }
+}
---
+++ C#_Basic/Practice_24_03_21/Practice_02.cs
@@ -0,0 +1,65 @@
+﻿namespace MultiThreading
+{
+    
+    internal class Practice_02
+    {
+        const int MB = 1024 * 1024;
+
+        public static Func<int, int, int> Operations;
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public void Main()
+        {
+            Sum(1, 2);
+            Operations += Sum;
+
+            Operations += (a, b) =>
+            {
+                return (a + b);
+            };
+
+            Thread t1 = new Thread(() => //스레드 생성
+            {
+                HireBarista("James").GoToWork();
+            }, 1 * MB); //최대 스택크기를 지정해줄 수 있다.
+
+            t1.Start(); //start를 해줘야 CPU에서 실행된다.
+        }
+
+        static Barista HireBarista(string nickname)
+        {
+            return new Barista(nickname);
+        }
+    }
+    public class Barista
+    {
+        public enum Beverage
+        {
+            None,
+            Aspresso,
+            Latte,
+            Lemonade
+        }
+        public Barista(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        private Random _random;
+
+        public void GoToWork()
+        {
+            Console.WriteLine($"바리스타 {Name}은 출근합니다....");
+        }
+
+        public Beverage MakeRandomBeverage()
+        {
+            return (Beverage)_random.Next(1, Enum.GetValues(typeof(Beverage)).Length);
+        }
+    }
+}
+
